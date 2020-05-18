3d290a685e4389f5c37c82ef93df4cd19f3393ed
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-18 08:26:21 -0300

    componente, propriedades, estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 export default function App() {  
+  const [projects, setProjects] = useState(['projeto 1', 'projeto 2']);
+
+
+  function handleAddProject() {
+    setProjects([...projects, `projeto ${Date.now()}`]);
+  }
+
   return (
-    /** Quando existe mais de um componente é necessário fazer um fragment <></> */
     <>
-      <Header />
-      <Header />
+      <Header title="Projects"/>
+      
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button onClick={handleAddProject}type="button">Add project</button>
+
     </>
   );
 }
